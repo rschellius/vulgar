32afceefe90de268b698d8a530da1de60362e622
Robin Schellius <r.schellius@avans.nl>
2016-10-15 08:29:11 +0200

    update

--- app/models/recipe.model.js
+++ app/models/recipe.model.js
@@ -16,25 +16,25 @@ import mongoose from 'mongoose';
 
 // Create a `schema` for the `Todo` object
 let recipeSchema = new mongoose.Schema({
-  title: { type : String },
-  tags: { type: Array },
-  rating: { type: Number},
-  creator: { type: String},
-  description: { type : String },
-  ingredients: [{
-    amount: {
-      type: String
-    },
+    title: { type: String },
+    tags: { type: Array },
+    rating: { type: Number },
+    creator: { type: String },
+    description: { type: String },
+    ingredients: [{
+        amount: {
+            type: String
+        },
 
-    unit: {
-      type: String
-    },
+        unit: {
+            type: String
+        },
 
-    name: {
-      type: String
-    }
-  }],
-  directions: { type: Array }
+        name: {
+            type: String
+        }
+    }],
+    directions: { type: Array }
 });
 
 // Expose the model so that it can be imported and used in
